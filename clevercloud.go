6ee65a951604e78b76c2cd59f71aaccd4d3ad8a0
Loic Tosser <loic@kalvad.com>
2020-01-27 10:55:24 +0400

    CleverCloud integration

---
+++ clevercloud.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"github.com/streadway/amqp"
+)
+
+type CleverCloudMessage struct {
+	Event     string `json:"event" binding:"required"`
+}
+
+func clevercloud(c *gin.Context) {
+	// Get the connection string from the environment variable
+	url := os.Getenv("AMQP_URL")
+	// Connect to the rabbitMQ instance
+	connection, err := amqp.Dial(url)
+	if err != nil {
+		panic("could not establish connection with RabbitMQ:" + err.Error())
+	}
+	secret := c.Param("secret")
+	waeProxySecret := os.Getenv("WAE_PROXY_SECRET")
+	if secret != waeProxySecret {
+		c.String(http.StatusForbidden, "Wrong Secret Key")
+		return
+	}
+	var json CleverCloudMessage
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create a channel from the connection. We'll use channels to access the data in the queue rather than the connection itself.
+	channel, err := connection.Channel()
+	
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not Connect to RabbitMQ")
+		return
+	}
+	typeNotification := "info"
+	if json.Event == "DEPLOYMENT_SUCCESS" {
+		typeNotification = "success"
+	}
+	if json.Event == "DEPLOYMENT_FAIL" {
+		typeNotification = "error"
+	}
+	message := amqp.Publishing{
+		Body: []byte(typeNotification),
+	}
+
+	// We publish the message to the exahange we created earlier
+	err = channel.Publish("wae", "wae", false, false, message)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not Connect to the Queue")
+		return
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"os"
 	"github.com/streadway/amqp"
-	"io/ioutil"
 )
 
 func main() {
@@ -26,41 +24,6 @@ func main() {
 	}
         channel.QueueBind(q.Name, "#", "wae", false, nil)	
 	router := gin.Default()
-	router.POST("/webhook/:secret", func(c *gin.Context) {
-		secret := c.Param("secret")
-		waeProxySecret := os.Getenv("WAE_PROXY_SECRET")
-		if secret != waeProxySecret {
-			c.String(http.StatusForbidden, "Wrong Secret Key")
-			return
-		}
-		var bodyBytes []byte
-		var err error
-		if c.Request.Body != nil {
-			bodyBytes, err = ioutil.ReadAll(c.Request.Body)
-			if err != nil {
-				c.String(http.StatusInternalServerError, "No Body")
-				return
-			}
-		}
-
-		// Create a channel from the connection. We'll use channels to access the data in the queue rather than the connection itself.
-		channel, err := connection.Channel()
-		
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Could not Connect to RabbitMQ")
-			return
-		}
-		message := amqp.Publishing{
-			Body: bodyBytes,
-		}
-
-		// We publish the message to the exahange we created earlier
-		err = channel.Publish("wae", "wae", false, false, message)
-
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Could not Connect to the Queue")
-			return
-		}
-	})
+	router.POST("/webhook/clevercloud/:secret", clevercloud)
 	router.Run(":8080")
 }
